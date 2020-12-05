4e77178055b2a79ecc0b9996e994060fd484e5f1
Meenalochini Thiyagarajan <meenalochini1805@gmail.com>
2020-12-05 18:56:45 -0500

    auto-complete implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
-
-
 import { AddressSplitComponent } from './address-split/address-split.component';
 import { AutoAddressComponent } from './auto-address/auto-address.component';
 
